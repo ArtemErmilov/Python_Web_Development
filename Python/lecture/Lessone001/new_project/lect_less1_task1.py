2cc59f921661cbe60ee34b3a1501665ef49397b4
Artem Ermilov <ermilo-artjom@rambler.ru>
2023-09-03 18:46:25 +0300

    Лекция 1

---
+++ Python/lecture/Lessone001/new_project/lect_less1_task1.py
@@ -0,0 +1,26 @@
+name = 'Alex'
+MAX_COUNT = 1000 # константа 
+ZERO = 0 # константа 
+age = None
+
+# a = 5
+# print(id(a))
+# a = "hello world"
+# print(id(a))
+# a = 42.0 * 3.141592 / 2.71828
+# print(id(a))
+
+# print(name, age, a, 456, 'text', sep='(=^.^=)', end='#')
+# print('Any txt')
+
+# res = input('Напиши свой текст: ')
+
+# print('Ты написал', res,sep=' ')
+
+ADULT = 18
+
+age = int (input ('Сколько тебе лет? '))
+
+how_oid = ADULT - age
+
+print(how_oid, 'Осталось до совершеннолетия', sep=' - ')
